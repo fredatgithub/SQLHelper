3ecd62c91784993aeac566225e66f539ad322d57
fredatgithub <domaine75@gmail.com>
2019-12-24 18:05:47 +0100

    Add unit test project

---
+++ UnitTestLibraryHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestLibraryHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestLibraryHelper")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("390dec0d-a6f8-4f7d-8afd-f3978870a38a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestLibraryHelper/UnitTestWindowsHelper.cs
@@ -0,0 +1,37 @@
+﻿using LibraryHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+
+namespace UnitTestLibraryHelper
+{
+  [TestClass]
+  public class UnitTestWindowsHelper
+  {
+    [TestMethod]
+    public void TestMethod_ClearListbox()
+    {
+      ListBox source = new ListBox();
+      source.Items.Add("item1");
+      source.Items.Add("item2");
+      WindowsHelper.ClearListbox(source);
+      int expected = 0;
+      int result = source.Items.Count;
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_DeleteSelectedItemsFromListbox()
+    {
+      ListBox source = new ListBox();
+      source.Items.Add("item1");
+      source.Items.Add("item2");
+      source.Items.Add("item3");
+      source.Items.Add("item4");
+      source.SetSelected(0, true);
+      WindowsHelper.DeleteSelectedItemsFromListbox(source);
+      int expected = 3;
+      int result = source.Items.Count;
+      Assert.AreEqual(result, expected);
+    }
+  }
+}
